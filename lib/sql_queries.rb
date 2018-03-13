ad28c039832e4897372b3addafcfb2f038c5eb92
Petya Djanovska <petya.djanovska@gmail.com>
2018-03-13 20:39:54 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,7 +19,7 @@ end
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
 "SELECT projects.title, SUM(pledges.amount) - SUM(projects.funding_goal) FROM projects, pledges
 WHERE SUM(pledges.amount) >= SUM(projects.funding_goal);"
-SELECT projects.title, SUM(pledges.amount) - projects.funding_goal as diff FROM projects, pledges HAVING projects.id = pledges.project_id AND diff >= '0' projects.id;
+SELECT projects.title, SUM(pledges.amount) - projects.funding_goal FROM projects, pledges WHERE projects.id = pledges.project_id AND diff >= '0' projects.id;
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
