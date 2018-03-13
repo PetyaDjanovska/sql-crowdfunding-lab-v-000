ab9334b34b22beb957b338d6c0381d8d835b2946
Petya Djanovska <petya.djanovska@gmail.com>
2018-03-13 21:54:52 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -22,6 +22,7 @@ WHERE SUM(pledges.amount) >= SUM(projects.funding_goal);"
 SELECT projects.title, SUM(pledges.amount) - projects.funding_goal FROM projects, pledges
 WHERE projects.id = pledges.project_id
 GROUP BY projects.id;
+
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
